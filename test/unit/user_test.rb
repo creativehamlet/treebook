3215dabd763dbb546dc5bed4b5c87c62b87c8694
creatiu <b@portf.com>
2013-03-18 19:22:27 -0400

    Fixed bug with user profile validation

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -34,4 +34,13 @@ class UserTest < ActiveSupport::TestCase
     assert !user.errors[:profile_name].empty?
     assert user.errors[:profile_name].include?("Must be formatted correctly.")
   end
+
+  test "a user can have a correctly formatted profile name" do
+    user = User.new(first_name: 'Brad', last_name: 'Sosnowski', email: 'brad2@teamtreehouse.com')
+    user.password = user.password_confirmation = 'asdfasdf'
+
+    user.profile_name = 'bradsosnowski_1'
+    assert user.valid?
+  end
+
 end
